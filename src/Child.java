69c8cfd9a5ebbd2587d3867f3975917dbf40c0d1
Graham <graham.hadden@gmail.com>
2019-05-19 13:48:38 +0100

    Commit 2

---
+++ src/Child.java
@@ -0,0 +1,5 @@
+public class Child extends Parent {
+    public void call() {
+        super.call(this);
+    }
+}
---
+++ src/Parent.java
@@ -0,0 +1,8 @@
+public class Parent {
+
+    protected int data = 5;
+
+    public void call(Child o) {
+        System.out.println("Data = " + o.data);
+    }
+}
